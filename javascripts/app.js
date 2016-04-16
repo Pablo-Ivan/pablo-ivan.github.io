b1272934b52fb647cc83cbb2bb5527e07134aeb3
Pablo Iván G. Soto <pabloivangomezsoto@gmail.com>
2016-04-16 16:22:47 -0500

    Added https.

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,3 +3,12 @@ var app = angular.module('principal', ['ngStorage']);
 app.controller('MainCtrl', function($scope, $localStorage) {
   $scope.$storage = $localStorage;
 });
+
+//HTTPS
+var host = "pablo-ivan.github.io";
+if ((host == window.location.host) && (window.location.protocol != "https:")) {
+    window.location.protocol = "https";
+}
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
