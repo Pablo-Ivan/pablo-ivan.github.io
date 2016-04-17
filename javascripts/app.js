d82960bdf9c787ee8d831e127a686339b5609b03
Pablo Iván G. Soto <pabloivangomezsoto@gmail.com>
2016-04-16 23:23:05 -0500

    Added scope.chat.

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,6 +2,7 @@
 var app = angular.module('principal', ['ngStorage']);
 app.controller('MainCtrl', function($scope, $localStorage) {
   $scope.$storage = $localStorage;
+  $scope.chat = '';
 });
 
 //HTTPS
