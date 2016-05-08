7a76d9ea8dcbe67b2e6ec347559e613cc0a29106
Pablo Iván G. Soto <pabloivangomezsoto@gmail.com>
2016-05-08 16:33:21 -0500

    Back to normality.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,12 +1,8 @@
-/* 
-AngularJS. Pablo Ivan G. Soto.
-Likely to be removed in future versions where Web Components allow me the same functionality.
-*/
-  var app = angular.module('principal', ['ngStorage']);
-  app.controller('MainCtrl', function($scope, $localStorage) {
-    $scope.$storage = $localStorage.$default({ nombre: '', edad: '', correo: ''});
-  });
-/*HTTPS
+var app = angular.module('principal', ['ngStorage']);
+app.controller('MainCtrl', function($scope, $localStorage) {
+  $scope.$storage = $localStorage.$default({ nombre: '', edad: '', correo: ''});
+});
+// HTTPS
 var host = "pablo-ivan.github.io";
 if ((host == window.location.host) && (window.location.protocol != "https:")) {
     window.location.protocol = "https";
@@ -14,4 +10,3 @@ if ((host == window.location.host) && (window.location.protocol != "https:")) {
 String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
-*/
