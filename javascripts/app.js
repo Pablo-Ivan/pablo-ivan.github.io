83bf76ff051db1e193153183137204ceaee12283
Pablo Iván G. Soto <pabloivangomezsoto@gmail.com>
2016-06-10 17:03:25 -0500

    Removed unnecesary.

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,11 +2,7 @@ var websiteApp = angular.module('principal', ['ngStorage']);
 websiteApp.controller('MainCtrl', function($scope, $localStorage) {
   $scope.$storage = $localStorage.$default({ nombre: '', edad: '', correo: ''});
 });
-// HTTPS
-var host = "pablo-ivan.github.io";
-if ((host == window.location.host) && (window.location.protocol != "https:")) {
-    window.location.protocol = "https";
-}
+
 String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
